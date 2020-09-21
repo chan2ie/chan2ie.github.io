9a9a182b4b5c4907b5a7c64fb73b3d42efdafe76
chan2ie <dl2cksgml@gmail.com>
2020-09-22 00:59:34 +0900

    ICC #2

---
+++ Intro_to_Creative_Computing/Sandy_Sketch/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+function keyPressed() {
+	
+  /*
+	if(key == 'w'){
+		print("it's w!!!");	
+	}
+  */
+	
+	//print("KeyCode: " + keyCode);
+	//print("Key: " + key);
+	
+	
+}
